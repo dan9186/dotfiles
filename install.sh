d1e482a7e7a495fb47efbdffc4ce3d1abbd46230
Daniel Hess <dan9186@gmail.com>
2016-10-21 13:57:36 -0700

    simplifying install script

--- install.sh
+++ install.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-if [ $(hash git 2>/dev/null) ]; then
-	ln -s $PWD/gitconfig $HOME/.gitconfig
+function link_file () {
+	if (( $# > 0 )); then
+		FILE=$1
+		if [ -f "$HOME/.$FILE" ]; then
+			mv "$HOME/.$FILE" "$HOME/.$FILE.old"
+		fi
+		ln -s "$PWD/$FILE" "$HOME/.$FILE"
+	fi
+}
+
+if [ ! $(hash git 2>/dev/null) ]; then
+	link_file gitconfig
 fi
 
-if [ $(hash tmux 2>/dev/null) ]; then
-	ln -s $PWD/tmux.conf $HOME/.tmux.conf
+if [ ! $(hash tmux 2>/dev/null) ]; then
+	link_file tmux.conf
 fi
 
-if [ $(hash zsh 2>/dev/null) ]; then
-	ln -s $PWD/zprofile $HOME/.zprofile
-	ln -s $PWD/zshrc $HOME/.zshrc
+if [ ! $(hash zsh 2>/dev/null) ]; then
+	link_file zprofile
+	link_file zshrc
 fi
